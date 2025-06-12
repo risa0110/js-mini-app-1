8ae2d168cab197f079e9181746764cd95b02f8b3
Risa Hasegawa <r0121m1014@gmail.com>
2025-06-11 17:35:38 -0700

    add the function of checking the card match & the clear message alert

--- animation.js
+++ animation.js
@@ -1,18 +1,34 @@
-//Flip a card when it clicked
 var cards = document.getElementsByClassName("card");
-for(var i=0; i<cards.length; i++){
-    cards[i].addEventListener("click", function(){
-    var flippedCards = document.querySelectorAll(".card.flipped")
-    
-    //Allow only 2 cards to be flipped
-    if (flippedCards.length == 2 && !this.classList.contains("flipped")){
-        return;
-    }
-    this.classList.toggle("flipped");
-})};
+let flippedCards = [];
+for (var i = 0; i < cards.length; i++) {
+    //Flip a card when it clicked
+    cards[i].addEventListener("click", function () {
+        this.classList.add("flipped");
+        flippedCards.push(this);
+        //Allow only 2 cards to be flipped
+        if (flippedCards.length === 2) {
+            let text1 = flippedCards[0].querySelector(".card-back").textContent;
+            let text2 = flippedCards[1].querySelector(".card-back").textContent;
+            //Check if the 2 cards match
+            //If not matched, flip it back
+            if (text1 !== text2) {
+                setTimeout(function () {
+                    flippedCards[0].classList.remove("flipped");
+                    flippedCards[1].classList.remove("flipped");
+                    flippedCards = [];
+                }, 800);
+            }
+            //If matched, keep it
+            else {
+                flippedCards = [];
 
-//Check if the 2 cards match
-  //If not matched, flip it back
-  //If matched, keep it
+                //Show a "game clear" message
+                if (document.querySelectorAll(".card.flipped").length === cards.length) {
+                    setTimeout(function () {
+                        alert("GAME CLEAR!");
+                    }, 500);
+                }
+            }
+        }})
+};
 
-  //Show a "game clear" message
